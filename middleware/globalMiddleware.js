0d39147b286ee0198f13c79770450600b1092e7f
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2019-12-20 14:58:32 -0600

    fixing authentication issue

--- middleware/globalMiddleware.js
+++ middleware/globalMiddleware.js
@@ -9,5 +9,5 @@ module.exports = (server) => {
 	server.use(helmet());
 	server.use(express.json());
 	server.use(morgan('short'));
-	server.use(cors());
+	server.use(cors({ credentials: true}));
 };
